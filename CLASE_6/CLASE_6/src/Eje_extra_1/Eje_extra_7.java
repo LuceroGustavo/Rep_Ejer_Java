d96358a5cab31d6096b25fd35eb9842f0ae4a64b
Gustavo Lucero <lucerogustavosi@gmail.com>
2023-04-16 19:15:19 -0300

    5 commit

---
+++ CLASE_6/CLASE_6/src/Eje_extra_1/Eje_extra_7.java
@@ -0,0 +1,24 @@
+/*
+ Realice un programa que calcule y visualice el valor máximo, el valor
+mínimo y el promedio de n números (n>0). El valor de n se solicitará al
+principio del programa y los números serán introducidos por el usuario.
+Realice dos versiones del programa, una usando el bucle “while” y otra
+con el bucle “do - while”.
+ */
+
+
+package Eje_extra_1;
+
+import java.util.Scanner;
+
+public class Eje_extra_7 {
+Scanner leer = new Scanner(System.in);
+     
+in num;
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
